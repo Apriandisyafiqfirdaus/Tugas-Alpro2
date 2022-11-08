f46a3410690e9993c66bb77e7bf9126d7aab6e8f
Apriandisyafiqfirdaus <friangamal@gmail.com>
2022-11-08 19:57:17 +0700

    Menambah File

---
+++ jawaban.java
@@ -0,0 +1,14 @@
+import java.util.Scanner; // import the Scanner class
+class  jawaban  {
+public static void main(String[] args) {
+
+Scanner myObj = new Scanner(System.in);
+String userName;
+
+// Enter username and press Enter
+
+System.out.println("Apriandi Syafiq Firdaus");
+userName = myObj.nextLine();
+System.out.println("Username is: " + userName);
+}
+}
